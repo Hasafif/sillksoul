d555ab723be709ff61566529018e7cd05d39cb05
Hasafif <hassan.n.afif@gmail.com>
2025-05-26 21:41:43 -0400

    first commit

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { useTranslation } from "../hooks/useTranslation";
 
-
-
 const AboutSection = () => {
   const { t } = useTranslation();
   const [isHovered, setIsHovered] = useState(false);
